82305a115f2eb5e3d2b113a4db67a1c6410e2498
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-17 09:19:59 +0200

    add pokemon names into list

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,17 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then(data => data.json())
+    .then(data => {
+      for (let counter in data.results) {
+        const pokemonName = data.results[counter].name;
+        const li = document.createElement("li");
+        li.innerText = pokemonName;
+        ul.appendChild(li);
+      }
+    });
+
+  
 });
